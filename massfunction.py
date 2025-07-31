36129e64643d3893ba240aee5a5384a6ae3a8233
MChruslinska <144791439+Mchruslinska@users.noreply.github.com>
2025-07-31 14:55:29 +0200

    Update massfunction.py
    add integral_mass
    add self.bounds_normalize
    compute normalization_constant using bounds_normalize; solve the simple integral explicitly as in integral_number instead of calling quad (probably faster)

--- massfunction.py
+++ massfunction.py
@@ -3,34 +3,48 @@ from scipy.integrate import quad
 
 
 class MassFunction:
-    def __init__(self, alpha, total_mass, M_min, M_max_theory, M_max_sampled):
+    def __init__(self, alpha, total_mass, M_min_theory, M_max_theory, M_min_sampled, M_max_sampled):
         self.alpha = alpha
         self.total_mass = total_mass
-        self.M_min = M_min
+        self.M_min_theory = M_min_theory
+        self.M_min_sampled = M_min_sampled
         self.M_max_theory = M_max_theory
         self.M_max_sampled = M_max_sampled
 
     @property
     def bounds_theory(self):
-        return (self.M_min, self.M_max_theory)
+        return (self.M_min_theory, self.M_max_theory)
 
     @property
     def bounds_sampled(self):
-        return (self.M_min, self.M_max_sampled)
+        return (self.M_min_sampled, self.M_max_sampled)
 
+    @property
+    def bounds_normalize(self):
+        return (self.M_min_theory, self.M_max_sampled)
+        
     def normalization_constant(self):
-        a, b = self.bounds_theory
-        if self.alpha == 2:
-            integral_mass, _ = quad(lambda m: m**(-1), a, b)
+        m1, m2 = self.bounds_normalize
+        if self.alpha == 2.:
+            integral_mass = (np.log(m2)-np.log(m1))
         else:
-            integral_mass, _ = quad(lambda m: m * m**(-self.alpha), a, b)
-        return self.total_mass / integral_mass
+            integral_mass = (m2**(2.-self.alpha) - m1**(2.-self.alpha))/(2.-self.alpha)
+        return  self.total_mass / integral_mass
 
     def integral_number(self, m1, m2):
         k = self.normalization_constant()
         if self.alpha == 1:
             return k * np.log(m2 / m1)
         return k * (m2**(1 - self.alpha) - m1**(1 - self.alpha)) / (1 - self.alpha)
+
+    def integral_mass(self, m1, m2 ):
+        norm = self.normalization_constant()        
+        if self.alpha == 2.:
+            M_bin = norm*(np.log(m2)-np.log(m1))
+        else:
+            M_bin = norm*(m2**(2.-self.alpha) - m1**(2.-self.alpha))/(2.-self.alpha)
+        return M_bin
+        
 
     def inverse_integral(self, m_high, n_target):
         k = self.normalization_constant()
